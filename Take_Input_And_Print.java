10e486884ff7f34a214939c9ede070ed5a1c7ef5
Anjalimusireddy13 <143482854+Anjalimusireddy13@users.noreply.github.com>
2023-10-13 10:33:57 +0530

    import java.util.Scanner; public class num {     public static void main(String arrgs[])     {         Scanner sc=new Scanner(System.in);         String s1=sc.nextLine();         System.out.println(s1);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class num
+{
+    public static void main(String arrgs[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.nextLine();
+        System.out.println(s1);
+    }
+}

c43f509949c7e0800cac01a97be9892c06c39989
Anjalimusireddy13 <143482854+Anjalimusireddy13@users.noreply.github.com>
2023-10-20 11:33:46 +0530

    import java.util.*; public class weight {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int a=sc.nextInt();         int b=sc.nextInt();         int c=((x*3)-(a+b));         System.out.printf("%d",c);     } }

---
+++ Average_weight.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class weight
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=((x*3)-(a+b));
+        System.out.printf("%d",c);
+    }
+}
